3ee428930ff335d26aea8a23078ac3a0f55c6fad
Tiago Nascimento <tiago.nascimento@spruceid.com>
2022-02-16 15:34:36 -0300

    Remove scope on route mounting
    Signed-off-by: Tiago Nascimento <tiago.nascimento@spruceid.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 SiweRails::Engine.routes.draw do
-  scope 'siwe' do
-    root to: 'application#index'
+  root to: 'application#index'
 
-    post 'message' => 'application#message'
-    post 'signature' => 'application#signature'
-  end
+  post 'message' => 'application#message'
+  post 'signature' => 'application#signature'
 end
